35f61e8eff681d4055eec3eb803b813b62faad48
ajayraj201651006 <ajayraj003016@gmail.com>
2019-06-11 21:52:24 +0530

    connect redux with react

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+const intialState = {
+    counter: 0
+}
+
+const reducer = (state = intialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+     return state;
+}
+
+export default reducer;
