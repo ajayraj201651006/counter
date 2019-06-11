1b3059159a95659dc42f5a35455b1840afface1b
ajayraj201651006 <ajayraj003016@gmail.com>
2019-06-11 22:03:24 +0530

    create actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,24 @@ const reducer = (state = intialState, action) => {
             counter: state.counter + 1
         }
     }
+
+    else if(action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    else if(action.type === 'ADD') {
+        return {
+            counter: state.counter + action.value
+        }
+    }
+
+    else if(action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.value
+        }
+    }
      return state;
 }
 
